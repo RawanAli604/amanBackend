e0777ecb54335b8b1372e7f3403c92436576d92f
RawanAli <rawan.ali.fardan@email.com>
2025-11-09 00:37:19 +0300

    read route user.js

--- controllers/users.js
+++ controllers/users.js
@@ -4,37 +4,16 @@ const router = express.Router();
 
 const User = require('../models/user');
 
-router.get('/', async (req, res) => {
+//get user profile
+router.get('/:userId', async (req, res) => {
   try {
-    // Get a list of all users, but only return their username and _id
-    const users = await User.find({}, 'username');
-
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ err: err.message });
+    const user = await User.findById(req.params.userId).select('-hashedPassword');
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
-
-router.get('/current-user', async (req, res) => {
-  try {
-    // Get a list of all users, but only return their username and _id
-    const user = await User.findById(req.user._id);
-
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
-});
-
-router.get('/:id', async (req, res) => {
-  try {
-    // Get a list of all users, but only return their username and _id
-    const user = await User.findById(req.params.id);
-
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
-});
-
 module.exports = router;
