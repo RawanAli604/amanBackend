76403eb4600b4e3e434e3cf7456ffd9f4753588d
alaalshehabii <alaa.hsan.hajj@gmail.com>
2025-11-10 19:23:53 +0300

    created mongoose schema

---
+++ models/comment.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const commentSchema = mongoose.Schema({
+    report_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Report',
+        required: true,
+    },
+})
