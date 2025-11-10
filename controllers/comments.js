024bbf90bad439e28f67f7d51c063122306fd5f1
alaalshehabii <alaa.hsan.hajj@gmail.com>
2025-11-10 20:53:07 +0300

    Add POST route to create a comment

--- controllers/comments.js
+++ controllers/comments.js
@@ -4,4 +4,21 @@ const router = express.Router();
 const Comment = require('../models/comment');
 const verifyToken = require('../middleware/verify-token');
 
+router.post('/', verifyToken, async (req, res) => {
+    try {
+        const { report_id, comment_text } = req.body;
+        const comment = await Comment.create({
+            report_id,
+            user_id: req.user._id,
+            comment_text,
+        });
+
+        res.status(201).json(comment);
+    } catch (err) {
+        res.status(500).json({message: 'Error Creating comment', error: err.message });
+
+    }
+    });
+    
+})
 
